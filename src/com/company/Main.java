71863fb0e9aab8dd9f817f507e5271f8a53d8540
Pio <lainiamali@gmail.com>
2021-06-15 14:36:02 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// using println
+        System.out.println(" Hi, ich bin Pio");
+        System.out.printf("I like to programming");
+        System.out.printf("I have been working with Java since the late 2003's");
+        System.out.printf("It has changed a lot in that time ");
+    }
+}
